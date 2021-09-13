c28f80f8057d6dca8ed3ba5746e91df9cf68d1c0
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-09-13 22:27:31 +0300

    Converter projesine başlandı.

---
+++ src/BusinessDetective/BusinessConverter/InterfaceType.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace BusinessConverter
+{
+    internal class InterfaceType
+    {
+        public string Name { get; internal set; }
+        public SyntaxNode FormattedFile { get; internal set; }
+        public string[] DirectoryName { get; internal set; }
+    }
+}
---
+++ src/BusinessDetective/BusinessConverter/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.CodeAnalysis.MSBuild;
+using System.Threading.Tasks;
+
+namespace BusinessConverter
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            MSBuildWorkspace msWorkspace = MSBuildWorkspace.Create();
+            msWorkspace.LoadMetadataForReferencedProjects = true;            
+        }
+    }
+}
